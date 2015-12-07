4be4b67b045605c7a2cfed5ab660312f353ad44e
stajs <shannonsnz@gmail.com>
2015-12-07 22:18:59 +1300

    Init

---
+++ EfPermissions/src/EfPermissions.Service/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using static System.Console;
+
+namespace EfPermissions.Service
+{
+	public class Program : ServiceBase
+	{
+		public static void Main(string[] args)
+		{
+			WriteLine("EfPermissions");
+			ReadKey();
+		}
+	}
+}
